3a1fe46f4080f356c18200322141bb2625bac0cd
Antoine Seguin <20756073+antt0n@users.noreply.github.com>
2025-06-06 19:08:33 +0200

    Update demo to use auto detection

--- demo/index.ts
+++ demo/index.ts
@@ -1,20 +1,22 @@
-import { createLaunchpadCore, autoDetectLaunchpadCore } from "../src/";
+import { autoDetectLaunchpadCore } from '../src';
 
-//const App = createLaunchpadCore("LaunchpadX");
-const App = autoDetectLaunchpadCore();
+(async () => {
+  const app = await autoDetectLaunchpadCore();
+  app.on('onConnected', (instance, driver) => {
+    // instance.out.send(driver.textScrolling(15, 'Welcome!'))
+    instance.out.send(driver.programmerToggle(true));
+    instance.out.noteOn(0, 11, 25); // Pad 11 to color 25
+  });
+  app.on('onMidiIn', (data) => {
+    console.log(data);
+  });
 
-App.on("onConnected", (instance, driver) => {
-    console.log(instance.out.info());
-    //instance.out.send(driver.textScrolling(15, "Welcome!"))
-    instance.out.send(driver.programmerToggle(true))
-    instance.out.noteOn(0, 11, 25) // Pad 11 to color 25
-})
-
-App.on("onMidiIn", (data) => {
-    console.log(data)
-})
-
-App.on("onDisabled", (instance, driver) => {
+  app.on('onDisabled', (instance, driver) => {
+    instance.out.send(driver.textScrolling(15, 'Goodbye!'));
+    instance.out.send(driver.programmerToggle(false));
+    console.log('Shutdown...');
+  });
+})();
     instance.out.send(driver.textScrolling(15, "Goodbye!"))
     instance.out.send(driver.programmerToggle(false))
     console.log("Shutdown...")
