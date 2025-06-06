69afbf0f0ab0dfde14896df8ba74c021a179c6d8
Antoine <20756073+antt0n@users.noreply.github.com>
2025-06-06 18:01:08 +0200

    edit demo/index.ts

--- demo/index.ts
+++ demo/index.ts
@@ -1,3 +1,16 @@
-import { LaunchpadCore } from "../src"
+import { createLaunchpadCore } from "../src/";
 
-const App = new LaunchpadCore("LaunchpadX");
+const App = createLaunchpadCore("LaunchpadX");
+
+App.on("onConnected", (instance, driver) => {
+    instance.out.send(driver.textScrolling(15, "Welcome!"))
+    instance.out.noteOn(0, 11, 25) // Pad 11 to color 25
+})
+
+App.on("onMidiIn", (data) => {
+    console.log(data)
+})
+
+App.on("onDisabled", () => {
+    console.log("Shutdown...")
+})
